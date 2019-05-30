5987f0ebe331e6bc9c977a3f7b13c53433d9d516
Spencer Carli <spencercarli@gmail.com>
2019-05-30 09:54:52 -0500

    06 Modal Style Navigation

--- App/index.js
+++ App/index.js
@@ -1,3 +1,71 @@
-import Details from "./screens/Details";
+import React from "react";
+import { TouchableOpacity, Image, StatusBar } from "react-native";
+import { createAppContainer, createStackNavigator } from "react-navigation";
 
-export default Details;
+import Details from "./screens/Details";
+import Search from "./screens/Search";
+
+const HeaderRightButton = ({ onPress, style, icon }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Image
+      source={icon}
+      resizeMode="contain"
+      style={[
+        {
+          marginRight: 10,
+          width: 20,
+          height: 20,
+          tintColor: "#fff"
+        },
+        style
+      ]}
+    />
+  </TouchableOpacity>
+);
+
+const AppStack = createStackNavigator(
+  {
+    Details: {
+      screen: Details,
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: "Details",
+        headerRight: (
+          <React.Fragment>
+            <StatusBar barStyle="light-content" />
+            <HeaderRightButton
+              icon={require("./assets/search.png")}
+              onPress={() => navigation.navigate("Search")}
+            />
+          </React.Fragment>
+        ),
+        headerStyle: {
+          backgroundColor: "#3145b7",
+          borderBottomColor: "#3145b7"
+        },
+        headerTintColor: "#fff"
+      })
+    },
+    Search: {
+      screen: Search,
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: "Search",
+        headerRight: (
+          <React.Fragment>
+            <StatusBar barStyle="dark-content" />
+            <HeaderRightButton
+              icon={require("./assets/close.png")}
+              onPress={() => navigation.pop()}
+              style={{ tintColor: "#000" }}
+            />
+          </React.Fragment>
+        ),
+        headerLeft: null
+      })
+    }
+  },
+  {
+    mode: "modal"
+  }
+);
+
+export default createAppContainer(AppStack);
