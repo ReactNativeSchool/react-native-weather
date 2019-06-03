05c199a4fc9a055d03f3a231b820913babe3e9c9
Spencer Carli <spencercarli@gmail.com>
2019-06-03 09:41:21 -0500

    07 UI: Displaying Current Weather Data

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { ActivityIndicator, ScrollView, SafeAreaView } from "react-native";
+
 import { weatherApi } from "../util/weatherApi";
+import { Container } from "../components/Container";
+import { WeatherIcon } from "../components/WeatherIcon";
+import { BasicRow } from "../components/List";
+import { H1, H2 } from "../components/Text";
 
 export default class Details extends React.Component {
+  state = {
+    currentWeather: {},
+    loadingCurrentWeather: true
+  };
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
       console.log("position", position);
@@ -14,6 +25,12 @@ export default class Details extends React.Component {
     weatherApi("/weather", { zipcode, coords })
       .then(response => {
         console.log("current response", response);
+
+        this.props.navigation.setParams({ title: response.name });
+        this.setState({
+          currentWeather: response,
+          loadingCurrentWeather: false
+        });
       })
       .catch(err => {
         console.log("current error", err);
@@ -29,6 +46,32 @@ export default class Details extends React.Component {
       });
 
   render() {
-    return null;
+    if (this.state.loadingCurrentWeather) {
+      return (
+        <Container>
+          <ActivityIndicator color="#fff" size="large" />
+        </Container>
+      );
+    }
+
+    const { weather, main } = this.state.currentWeather;
+
+    return (
+      <Container>
+        <ScrollView>
+          <SafeAreaView>
+            <WeatherIcon icon={weather[0].icon} />
+            <H1>{`${Math.round(main.temp)}°`}</H1>
+            <BasicRow>
+              <H2>{`Humidity: ${main.humidity}%`}</H2>
+            </BasicRow>
+            <BasicRow>
+              <H2>{`Low: ${Math.round(main.temp_min)}°`}</H2>
+              <H2>{`High: ${Math.round(main.temp_max)}°`}</H2>
+            </BasicRow>
+          </SafeAreaView>
+        </ScrollView>
+      </Container>
+    );
   }
 }
