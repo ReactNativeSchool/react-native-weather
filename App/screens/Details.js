fc4a9edeb024d413206a1c526b62f05a175e9fa8
Spencer Carli <spencercarli@gmail.com>
2019-06-03 09:59:05 -0500

    08 UI: Displaying Forecast Weather Data

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -1,21 +1,57 @@
 import React from "react";
-import { ActivityIndicator, ScrollView, SafeAreaView } from "react-native";
+import {
+  ActivityIndicator,
+  ScrollView,
+  SafeAreaView,
+  View
+} from "react-native";
+import { format } from "date-fns";
 
 import { weatherApi } from "../util/weatherApi";
 import { Container } from "../components/Container";
 import { WeatherIcon } from "../components/WeatherIcon";
 import { BasicRow } from "../components/List";
-import { H1, H2 } from "../components/Text";
+import { H1, H2, P } from "../components/Text";
+
+const groupForecastByDay = list => {
+  const data = {};
+
+  list.forEach(item => {
+    const [day] = item.dt_txt.split(" ");
+    if (data[day]) {
+      if (data[day].temp_max < item.main.temp_max) {
+        data[day].temp_max = item.main.temp_max;
+      }
+
+      if (data[day].temp_min > item.main.temp_min) {
+        data[day].temp_min = item.main.temp_min;
+      }
+    } else {
+      data[day] = {
+        temp_min: item.main.temp_min,
+        temp_max: item.main.temp_max
+      };
+    }
+  });
+
+  const formattedList = Object.keys(data).map(key => ({
+    day: key,
+    ...data[key]
+  }));
+
+  return formattedList;
+};
 
 export default class Details extends React.Component {
   state = {
     currentWeather: {},
-    loadingCurrentWeather: true
+    loadingCurrentWeather: true,
+    forecast: [],
+    loadingForecast: true
   };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log("position", position);
       this.getCurrentWeather({ coords: position.coords });
       this.getForecast({ coords: position.coords });
     });
@@ -24,8 +60,6 @@ export default class Details extends React.Component {
   getCurrentWeather = ({ zipcode, coords }) =>
     weatherApi("/weather", { zipcode, coords })
       .then(response => {
-        console.log("current response", response);
-
         this.props.navigation.setParams({ title: response.name });
         this.setState({
           currentWeather: response,
@@ -39,14 +73,17 @@ export default class Details extends React.Component {
   getForecast = ({ zipcode, coords }) =>
     weatherApi("/forecast", { zipcode, coords })
       .then(response => {
-        console.log("forecast response", response);
+        this.setState({
+          loadingForecast: false,
+          forecast: groupForecastByDay(response.list)
+        });
       })
       .catch(err => {
         console.log("forecast error", err);
       });
 
   render() {
-    if (this.state.loadingCurrentWeather) {
+    if (this.state.loadingCurrentWeather || this.state.loadingForecast) {
       return (
         <Container>
           <ActivityIndicator color="#fff" size="large" />
@@ -69,6 +106,23 @@ export default class Details extends React.Component {
               <H2>{`Low: ${Math.round(main.temp_min)}°`}</H2>
               <H2>{`High: ${Math.round(main.temp_max)}°`}</H2>
             </BasicRow>
+
+            <View style={{ paddingHorizontal: 10, marginTop: 20 }}>
+              {this.state.forecast.map(day => (
+                <BasicRow
+                  key={day.day}
+                  style={{ justifyContent: "space-between" }}
+                >
+                  <P>{format(new Date(day.day), "dddd, MMM D")}</P>
+                  <View style={{ flexDirection: "row" }}>
+                    <P style={{ fontWeight: "700", marginRight: 10 }}>
+                      {Math.round(day.temp_max)}
+                    </P>
+                    <P>{Math.round(day.temp_min)}</P>
+                  </View>
+                </BasicRow>
+              ))}
+            </View>
           </SafeAreaView>
         </ScrollView>
       </Container>
