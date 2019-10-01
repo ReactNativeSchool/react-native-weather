0a96f5114062bd28dc99a7f0c866ca7c2e86d829
Spencer Carli <spencercarli@gmail.com>
2019-10-01 10:47:17 -0500

    Update deps

--- App/index.js
+++ App/index.js
@@ -3,6 +3,6 @@ import { View, Text } from "react-native";
 
 export default () => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>View the reference branch for finished code.</Text>
+    <Text>View the finished branch for finished code.</Text>
   </View>
 );
